9b407dcd20ec6e230869dd5b42dbb9519c3a3e63
Radu Ciurca <raduciurca@gmail.com>
2024-12-07 09:34:19 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProiectMPA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

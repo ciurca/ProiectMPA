12e94c8134e31c00216b6780ffb04820a9e8c1fb
Radu Ciurca <raduciurca@gmail.com>
2024-12-08 12:04:07 +0200

    Add shopping cart feature with session management
    Introduced a shopping cart feature allowing users to add, view, remove, and clear items. Configured session management to store cart data with a 30-minute idle timeout. Updated UI to provide feedback and navigation for the cart. Added `CartController` and `SessionHelper` for managing cart logic and session data.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
+using ProiectMPA.Helpers;
 using ProiectMPA.Models;
 using ProiectMPA.Models.Data;
 
@@ -10,6 +12,8 @@ namespace ProiectMPA.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly ProiectMPADbContext _context;
+
+        private const string CartSessionKey = "ShoppingCart";
 
         public HomeController(ILogger<HomeController> logger, ProiectMPADbContext context)
         {
@@ -21,6 +25,35 @@ namespace ProiectMPA.Controllers
         {
             var proiectMPADbContext = _context.Categories.Include(c => c.MenuItems);
             return View(await proiectMPADbContext.ToListAsync());
+        }
+
+        public IActionResult AddToCart(int menuItemId)
+        {
+            var cart = HttpContext.Session.GetObjectFromJson<List<OrderItem>>(CartSessionKey) ?? new List<OrderItem>();
+
+            var menuItem = _context.MenuItems.Find(menuItemId);
+
+            var cartItem = cart.Find(c => c.MenuItemId == menuItemId);
+            if (cartItem != null)
+            {
+                cartItem.Quantity++;
+                TempData["SuccessMessage"] = $"Updated the quantity of {menuItem.Name}. Current quantity is {cartItem.Quantity}.";
+            }
+            else
+            {
+                cart.Add(new OrderItem
+                {
+                    MenuItemId = menuItemId,
+                    MenuItem = menuItem,
+                    Quantity = 1
+                });
+                TempData["SuccessMessage"] = $"Successfully added {menuItem.Name} to the cart.";
+            }
+
+            HttpContext.Session.SetObjectAsJson(CartSessionKey, cart);
+
+
+            return RedirectToAction("Index");
         }
 
         public IActionResult Privacy()

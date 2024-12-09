b0cdea749f63d193511ec2a525bc978ae3f6e991
Radu Ciurca <raduciurca@gmail.com>
2024-12-09 18:37:19 +0200

    Add order status and details view
    Added Entity Framework Core to OrdersController.cs. Modified IndexAsync to include related entities and order by CreatedAt. Added Details method to OrdersController.cs. Updated Index.cshtml to enhance order display. Added Status property to Order model. Updated _Layout.cshtml with Orders link. Created Details.cshtml for order details. Added migration for Status column. Updated ProiectMPADbContextModelSnapshot.cs.

--- Areas/User/Controllers/OrdersController.cs
+++ Areas/User/Controllers/OrdersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProiectMPA.Models.Data;
 using ProiectMPA.Services;
 
@@ -17,10 +18,33 @@ namespace ProiectMPA.Areas.User.Controllers
             _context = context;
             _userService = userService;
         }
+
         public async Task<IActionResult> IndexAsync()
         {
             var user = await _userService.GetCurrentUser();
-            return View(_context.Orders.Where(o => o.User == user).ToList());
+            return View(await _context.Orders.Include(o => o.OrderItems).ThenInclude(oi => oi.MenuItem).Where(o => o.User == user).OrderByDescending(o => o.CreatedAt).ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _userService.GetCurrentUser();
+            var order = await _context.Orders
+                .Include(o => o.OrderItems)
+                .ThenInclude(oi => oi.MenuItem)
+                .Include(o => o.Statuses)
+                .FirstOrDefaultAsync(m => m.Id == id && m.UserId == user.Id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
         }
     }
 }
